1fa27e51dd6c2f728d01bfd7e4ce1e16dc049d98
Gilberto Stankiewicz <874134+kiewic@users.noreply.github.com>
2022-11-20 22:20:23 -0800

    Move header to AppComponent

--- src/app/station/station.component.ts
+++ src/app/station/station.component.ts
@@ -4,9 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-station',
   templateUrl: './station.component.html',
-  // styleUrls: ['./stations.component.css']
-  styles: [
-  ]
+  styleUrls: ['./station.component.css']
 })
 export class StationComponent implements OnInit {
   @Input()
